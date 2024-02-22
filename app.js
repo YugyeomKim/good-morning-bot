b8721e2516cbecbce7a80c44a06f65a23af22d36
YugyeomKim <hoodie0512@gmail.com>
2024-02-22 13:39:49 +0900

    Roll back message and adjust day

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const schedule = require("node-schedule");
 const axios = require("axios");
 const { format } = require("date-fns-tz");
+const { addDays } = require("date-fns");
 require("dotenv").config();
 
 const slackWebhookUrl = process.env.SLACK_WEBHOOK_URL;
@@ -11,19 +12,10 @@ const sayGoodMorning = schedule.scheduleJob(
   { hour: 6, minute: 30, tz: timeZone },
   async () => {
     const today = Date.now();
-    const formattedToday = format(today, "yy. MM. dd. (eee)", { timeZone });
+    const formattedToday = format(addDays(today, 1), "yy. MM. dd. (eee)");
     console.log("Good Morning, it's " + formattedToday);
     const response = await axios.post(slackWebhookUrl, {
-      blocks: [
-        {
-          type: "section",
-          text: {
-            type: "plain_text",
-            text: `<!channel>\n\n${formattedToday}\n굿모닝! :일출:\n1. 시작시간\n2. 종료 시간\n3. 업무 내용`,
-            emoji: true,
-          },
-        },
-      ],
+      text: `<!channel>\n\n${formattedToday}\n굿모닝! :일출:\n1. 시작시간\n2. 종료 시간\n3. 업무 내용`,
     });
     console.log(response.data);
   }
